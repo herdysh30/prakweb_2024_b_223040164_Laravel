e93dfb32b49463e3dc37a3ab0e65edb6300d15a8
herdysh30 <herdy.syaban@gmail.com>
2024-11-10 20:26:04 +0700

    6. View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,9 +11,50 @@ Route::get('/about', function () {
     return view('about',['name' => "Herdy Sya'banul Hakim",'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog',['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts',['title' => 'Blog','posts'=>[
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => "Herdy Sya'banul Hakim",
+            'body' =>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, autem qui dolor nam illo voluptatem numquam laborum cupiditate corporis. Repellendus ut sit adipisci voluptas illum nulla modi neque delectus vel et in maxime alias esse quidem, quas quasi possimus. Labore!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => "Herdy Sya'banul Hakim",
+            'body' =>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi cumque quaerat ex iure quae alias libero perspiciatis voluptatem possimus reiciendis aliquid nesciunt consequatur, eum magnam, obcaecati voluptatibus, quibusdam dolorem facilis!'
+        ]
+    ]]);
 });
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => "Herdy Sya'banul Hakim",
+            'body' =>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, autem qui dolor nam illo voluptatem numquam laborum cupiditate corporis. Repellendus ut sit adipisci voluptas illum nulla modi neque delectus vel et in maxime alias esse quidem, quas quasi possimus. Labore!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => "Herdy Sya'banul Hakim",
+            'body' =>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi cumque quaerat ex iure quae alias libero perspiciatis voluptatem possimus reiciendis aliquid nesciunt consequatur, eum magnam, obcaecati voluptatibus, quibusdam dolorem facilis!'
+        ]
+        ];
+
+        $post = Arr::first($posts, function($post) use ($slug){
+            return $post['slug'] == $slug;
+        });
+
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
 Route::get('/contact', function () {
     return view('contact',['title' => 'Contact']);
 });
